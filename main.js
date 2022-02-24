43715b51968ad3c743bbfc188f7f5fe65ae1738a
jade0819 <break0819@gmail.com>
2022-02-24 22:30:46 +0900

    JS Navbar menu click

--- main.js
+++ main.js
@@ -4,8 +4,8 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     } else {
@@ -13,3 +13,14 @@ document.addEventListener('scroll', () => {
     }
 })
 
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', () => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) return;
+    
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: 'smooth'}); 
+})
