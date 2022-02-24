544b34fb0ebc4ea20e05ca7d36cf61015d8d993b
jade0819 <break0819@gmail.com>
2022-02-25 04:16:57 +0900

    JS: remove active from nav.

--- main.js
+++ main.js
@@ -61,6 +61,12 @@ workBtnContainer.addEventListener('click', (e) => {
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
     if(filter == null) return;
 
+    // remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
     setTimeout( () => {
         projects.forEach( (project) => {
@@ -71,7 +77,7 @@ workBtnContainer.addEventListener('click', (e) => {
                 project.classList.add('invisible');
             }
         })
-        
+
         projectContainer.classList.remove('anim-out');
     }, 300)
 
